319068d3f5179ad501299496b4300a93a86a5111
sifulsidd <sifulsiddiki@gmail.com>
2024-02-14 17:26:48 -0500

    Added boundaries to the ship with movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,19 +11,33 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
         self.ship = Ship(self)
+        
 
 
     def run_game(self):
         while True:
             self._check_events()
             self._update_screen()
+            self.ship.update()
             self.clock.tick(60)
+            
 
 
     def _check_events(self):
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type ==pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+                    
     
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
