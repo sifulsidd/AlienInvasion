550cf87476108de3cfdc73bcc2ca9da1bb5441aa
sifulsidd <sifulsiddiki@gmail.com>
2024-02-15 01:29:30 -0500

    Added a lot of alien positions to the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,7 +33,22 @@ class AlienInvasion:
             
     def _create_fleet(self):
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_heght = alien.rect.size
+        current_x, current_y = alien_width, alien_heght
+
+        while current_y < (self.settings.screen_height - 3*alien_heght):
+            while current_x < (self.settings.screen_width - 2 * alien_width): #creates margin the 2*alien_width part
+                self._create_alien(current_x, current_y)
+                current_x += 2*alien_width
+            current_x = alien_width
+            current_y += 2 * alien_heght
+
+    def _create_alien(self, x_position, y_position):
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
 
     def _update_bullets(self):
         self.bullets.update()   
