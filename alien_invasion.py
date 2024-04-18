d97cdc05cc5cecb82037fc04ce1d745324669857
sifulsidd <sifulsiddiki@gmail.com>
2024-04-17 20:51:24 -0400

    Added functionality to start or restart game with letter p

--- alien_invasion.py
+++ alien_invasion.py
@@ -130,24 +130,30 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                elif event.type ==pygame.KEYDOWN:
+                elif event.type == pygame.KEYDOWN:
                     self._check_keydown_events(event)
+                    if event.key == pygame.K_p:
+                        self._start_game()
                 elif event.type == pygame.KEYUP:
                     self._check_keyup_events(event)
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_pos = pygame.mouse.get_pos()
                     self._check_play_button(mouse_pos)
+                    
     
     def _check_play_button(self, mouse_pos):
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
-            self.stats.reset_stats()
-            self.game_active = True
-            self.bullets.empty()
-            self.aliens.empty()
-            self._create_fleet()
-            self.ship.center_ship()
-            pygame.mouse.set_visible(False)
+            self._start_game()
+    
+    def _start_game(self):
+        self.stats.reset_stats()
+        self.game_active = True
+        self.bullets.empty()
+        self.aliens.empty()
+        self._create_fleet()
+        self.ship.center_ship()
+        pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
